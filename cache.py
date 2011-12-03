4a1de1b374db0b2961a2e29e7e2b263660b29362
Limin Zhao <fdrex1987@gmail.com>
2011-12-03 08:27:44 +0800

    rewrite object_cache to support auto invalidation write cache codes in a new file

---
+++ cache.py
@@ -0,0 +1,128 @@
+# -*- coding: utf-8 -*-
+###################################################
+#this file is under GPL v3 license
+#Author: Rex  fdrex1987@gmail.com
+##################################################
+import logging
+import pickle
+from google.appengine.ext import db
+from google.appengine.api import memcache
+
+class ObjCache(db.Model):
+	cache_key = db.StringProperty(multiline=False)
+	value = db.BlobProperty()
+	#the following fields are used at cache auto invalidation
+	#divide the cases into the following basic categories:
+	depend_post_id = db.IntegerProperty(default=-1) #which page's is this cache depends on
+	depend_post_comments = db.IntegerProperty(default=-1) #which page's comments is this cache depends on (still use page id)
+	depend_url = db.StringProperty(default='') #which url is this cache depends on
+	depend_blog_roll = db.BooleanProperty(default=False) #is this cache depends on the blog roll
+
+	def invalidate(self):
+		memcache.delete(self.cache_key)
+		self.delete()
+
+	def update(self, new_value_obj):
+		memcache.set(self.cache_key,new_value_obj)
+		self.value = pickle.dumps(new_value_obj)
+		self.put()
+
+	@staticmethod
+	def invalidate(post_id=None,post_comments_id=None, url=None,blog_roll=None):
+		query = ObjCache.all()
+		if post_id is not None:
+			query = query.filter('depend_post_id =',post_id)
+		if post_comments_id is not None:
+			query = query.filter('depend_post_comments =',post_comments_id)
+		if url is not None:
+			query = query.filter('depend_url =',url)
+		if blog_roll is not None:
+			query = query.filter('depend_blog_roll =',blog_roll)
+		for obj in query:
+			obj.invalidate()
+
+	@staticmethod
+	def get(key_name):
+		result = memcache.get(key_name)
+		if result is not None:
+			return result
+		try:
+			result = ObjCache.all().filter('cache_key =',key_name).get()
+			if result is not None:
+				return pickle.loads(result.value)
+			else:
+				return None
+		except Exception, e:
+			logging.error(e.message)
+
+	@staticmethod
+	def create(key, value_obj, depend_post_id=-1,depend_comments_id=-1, depend_url='',depend_blog_roll=False):
+		try:
+			memcache.set(key,value_obj)
+			ObjCache(cache_key=key,
+					 value=pickle.dumps(value_obj),
+					 depend_post_id=depend_post_id,
+			         depend_comments_id = depend_comments_id,
+					 depend_url=depend_url,
+					 depend_blog_roll = depend_blog_roll
+					 ).put()
+		except Exception,e:
+			logging.error(e.message)
+
+	@staticmethod
+	def flush_all():
+		'''
+		This is faster than invalidate with default parameter values since memcache only need one call
+		'''
+		memcache.flush_all()
+		for cache in ObjCache.all():
+			cache.delete()
+
+def object_cache(key_prefix='',
+                 key_parameter_name='cache_key',
+                 depend_post_id_parameter_name='cache_depend_post_id',
+                 depend_post_comments_id_parameter_name='cache_depend_post_comments_id',
+                 depend_url_parameter_name='cache_depend_url',
+                 depend_blog_roll_parameter_name='cache_depend_blog_roll',
+                 cache_control_parameter_name='cache_control'):
+	'''
+	available options for cache control are: no_cache, cache
+	default option is cache
+	'''
+	def _decorate(method):
+		def _wrapper(*args, **kwargs):
+			key = key_prefix
+			if key_parameter_name in kwargs:
+				key = key+'_'+kwargs[key_parameter_name]
+				del kwargs[key_parameter_name]
+
+			cache_args = {}
+			pd = {
+				depend_post_id_parameter_name:'depend_post_id',
+			    depend_post_comments_id_parameter_name:'depend_post_comments',
+			    depend_url_parameter_name:'depend_url',
+			    depend_blog_roll_parameter_name:'depend_blog_roll',
+			}
+			for parameter_name in pd:
+				if parameter_name in kwargs:
+					cache_args[pd[parameter_name]] = kwargs[parameter_name]
+					del kwargs[parameter_name]
+					
+			cache_control = 'cache'
+			if cache_control_parameter_name in kwargs:
+				cache_control = kwargs[cache_control_parameter_name]
+				del kwargs[cache_control_parameter_name]
+
+			if cache_control == 'no cache':
+				return method(*args, **kwargs)
+
+			result = ObjCache.get(key)
+			if result is not None:
+				return result
+			
+			result = method(*args, **kwargs)
+			ObjCache.create(key,result,**cache_args)
+			return result
+
+		return _wrapper
+	return _decorate
